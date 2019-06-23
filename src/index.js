5f349fe2ebe1c561fd0bea51f02e8b0f909d4fbe
Manmohan Singh <manmohan.singh@publicissapient.com>
2019-06-23 14:29:18 +0100

    webpack hot module replacement plugin added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import logMessage from './js/logger'
 import './css/style.css'
+
 // Log message to console
 logMessage('Welcome to Expack!')
+
+// Needed for Hot Module Replacement
+if (typeof(module.hot) !== 'undefined') {
+    module.hot.accept()
+}
--- src/server/server-dev.js
+++ src/server/server-dev.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from 'express'
 import webpack from 'webpack'
 import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../../webpack.dev.config'
 
 const app = express()
@@ -14,6 +15,8 @@ const compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
 }))
+
+app.use(webpackHotMiddleware(compiler))
 
 app.get('*', (req, res) => {
     res.sendFile(HTML_FILE)
